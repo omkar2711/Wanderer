e5d6dea126b5e18f9b6764514c975d0dd64e6efa
Omkar Bharitkar <75433632+omkar2711@users.noreply.github.com>
2023-07-25 00:08:32 +0530

    Add files via upload

---
+++ requirements/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function omkar27111 (){
+    return (
+        <>
+            {/* API Key for google maps 
+            API key for travel advisor
+            API key for open weather (optional) */}
+        </>
+    )
+}
+
+export default omkar27111;
