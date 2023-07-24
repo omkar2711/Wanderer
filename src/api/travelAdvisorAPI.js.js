f6a88ec54983b721a0bcb9cf62da476caed1c5fa
Omkar Bharitkar <75433632+omkar2711@users.noreply.github.com>
2023-07-25 00:27:51 +0530

    Update travelAdvisorAPI.js.js

--- src/api/travelAdvisorAPI.js.js
+++ src/api/travelAdvisorAPI.js.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getPlacesData = async (type, sw, ne) => {
   try {
-    const { data: { data } } = await axios.get(`https://university-college-list-and-rankings.p.rapidapi.com/${type}/universities`, {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
         bl_latitude: sw.lat,
         bl_longitude: sw.lng,
